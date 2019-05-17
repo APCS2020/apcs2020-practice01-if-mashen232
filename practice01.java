82d6fefcfc9fa241ba8a619e40d682ea47ec746e
马宇鹏Mike <48340629+mashen232@users.noreply.github.com>
2019-05-17 11:08:22 +0800

    Mike practice01
    deep dark

---
+++ practice01.java
@@ -0,0 +1,19 @@
+public class practice{
+    public static String weekday(int num){
+        if(d ==1){
+            return "Monday";
+        } else if(d == 2){
+            return "Tuesday";
+        }else if(d == 3){
+            return "Wednesday";
+        }else if(d == 4){
+            return "Thursday";
+        }else if(d == 5){
+            return "Friday";
+        }else if(d == 6){
+            return "Saturday";
+        }else if(d == 7){
+            return "Sunday";
+        }
+    }
+}
